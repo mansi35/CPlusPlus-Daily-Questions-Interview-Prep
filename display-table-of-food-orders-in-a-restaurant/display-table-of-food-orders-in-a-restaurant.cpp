b8192e92d256b181756bbf942b7de929956f8e7c
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-02-06 09:59:27 +0530

    Time: 216 ms (54.65%), Space: 101.1 MB (12.03%) - LeetHub

---
+++ display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<string>> displayTable(vector<vector<string>>& orders) {
+        unordered_map<int, unordered_map<string, int>> table;
+        unordered_map<int, unordered_map<string, int> >::iterator itr; 
+        unordered_map<string, int>::iterator ptr;
+        set<string> foodItem;
+        set<int> tableNumber;
+        for (auto order: orders) {
+            foodItem.insert(order[2]);
+            tableNumber.insert(stoi(order[1]));
+            table[stoi(order[1])][order[2]] += 1;
+        }
+        vector<string> food;
+        for (auto item: foodItem)
+            food.push_back(item);
+        vector<int> tableNo;
+        for (auto no: tableNumber)
+            tableNo.push_back(no);
+        sort(food.begin(), food.end());
+        sort(tableNo.begin(), tableNo.end());
+        vector<vector<string>> result;
+        vector<string> header;
+        header.push_back("Table");
+        for (auto item: food)
+            header.push_back(item);
+        result.push_back(header);
+        for (auto number: tableNo) {
+            vector<string> row;
+            row.push_back(to_string(number));
+            for (auto f: food) {
+                row.push_back(to_string(table[number][f]));                
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+};

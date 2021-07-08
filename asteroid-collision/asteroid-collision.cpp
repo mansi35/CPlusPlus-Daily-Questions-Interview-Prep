a161df33b3169efe7a638ee6b07ec17519d5d045
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-07-08 23:08:15 +0530

    Time: 24 ms (14.17%), Space: 17.8 MB (54.42%) - LeetHub

---
+++ asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        s.push(asteroids[0]);
+        for (int i = 1; i < asteroids.size();) {
+            if (s.empty()) {
+                s.push(asteroids[i]);
+                i++;
+            } else if (s.top() > 0 and asteroids[i] < 0) {
+                if (abs(s.top()) == abs(asteroids[i])) {
+                    s.pop();
+                    i++;
+                } else if (abs(s.top()) < abs(asteroids[i])) {
+                    s.pop();
+                } else {
+                    i++;
+                }
+            } else {
+                s.push(asteroids[i]);
+                i++;
+            }
+        }
+        int size = s.size();
+        int* ans = new int[s.size()];
+        for (int i = s.size() - 1; i >= 0; i--) {
+            ans[i] = s.top();
+            s.pop();
+        }
+        vector<int> v(ans, ans+size);
+        return v;
+    }
+};

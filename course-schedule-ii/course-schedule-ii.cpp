ac2363e46e26e911edaf52887d4b2923feb15baf
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-07-09 18:03:03 +0530

    Time: 28 ms (29.40%), Space: 15.1 MB (19.44%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> umap;
+        vector<int> result;
+        for (auto edge: prerequisites) {
+            umap[edge[1]].push_back(edge[0]);
+        }
+        int* indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            indegree[i] = 0;
+        }
+        
+        for (auto list: umap) {
+            for (int i: list.second) {
+                indegree[i]++;
+            }
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int node = q.front();
+            result.push_back(node);
+            q.pop();
+            for (auto nbr: umap[node]) {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+        if (result.size() != numCourses) {
+            result.clear();  
+            return result;
+        }
+        return result;
+    }
+};

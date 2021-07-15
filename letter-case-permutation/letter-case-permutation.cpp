9c78ee2d2f5a683fc9e50ba6cc0cdce1954037cb
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-07-15 15:39:28 +0530

    Time: 4 ms (93.19%), Space: 11.5 MB (37.19%) - LeetHub

--- letter-case-permutation/letter-case-permutation.cpp
+++ letter-case-permutation/letter-case-permutation.cpp
@@ -1,23 +1,24 @@
 class Solution {
-public:
+public:    
+    vector<string> letterCasePermutation(string s) {
+        vector<string> allPermutations;
+        letterCasePermutationHelper(s, allPermutations, 0);
+        return allPermutations;
+    }
     
-    void letterCasePermutationHelper(string s, vector<string> &allPerms, int i) {
-        if (i > s.length())
-            return;
-        if (find(allPerms.begin(), allPerms.end(), s) == allPerms.end()) {
+     void letterCasePermutationHelper(string s, vector<string> &allPerms, int i) {
+        if (i == s.length()) {
             allPerms.push_back(s);
+            return;
         }
-        if (i+1 <= s.length()) {
+        
+        if (isdigit(s[i])) {
+            letterCasePermutationHelper(s, allPerms, i+1);
+        } else {
             s[i] = tolower(s[i]);
             letterCasePermutationHelper(s, allPerms, i+1);
             s[i] = toupper(s[i]);
             letterCasePermutationHelper(s, allPerms, i+1);
         }
-    }
-    
-    vector<string> letterCasePermutation(string s) {
-        vector<string> allPermutations;
-        letterCasePermutationHelper(s, allPermutations, 0);
-        return allPermutations;
     }
 };

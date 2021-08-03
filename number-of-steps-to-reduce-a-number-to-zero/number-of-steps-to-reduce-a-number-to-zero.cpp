a79dae055954f24f2bb8a90ca862bc0d6599103c
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-08-03 20:03:59 +0530

    Time: 228 ms (12.10%), Space: 301.3 MB (23.91%) - LeetHub

--- number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,13 +1,16 @@
 class Solution {
+    vector<int> dp = vector<int>(1000005, INT_MIN);
 public:
     int numberOfSteps (int num) {
         if (num == 0)
             return 0;
+        if (dp[num] != INT_MIN)
+            return dp[num];
         if (num % 2 == 0) {
-            return 1 + numberOfSteps(num/2);
+            return dp[num] = 1 + numberOfSteps(num/2);
         }
         else {
-            return 1 + numberOfSteps(num-1);
+            return dp[num] = 1 + numberOfSteps(num-1);
         }
     }
 };

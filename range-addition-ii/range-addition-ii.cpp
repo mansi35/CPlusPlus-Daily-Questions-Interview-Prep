97beecff20fbfb160b4015803f227030b6470873
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-08-30 13:22:37 +0530

    Time: 14 ms (12.11%), Space: 11.4 MB (25.37%) - LeetHub

--- range-addition-ii/range-addition-ii.cpp
+++ range-addition-ii/range-addition-ii.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    static bool compare1(vector<int>& a, vector<int>& b) {
-        return a[0] < b[0];
-    }
-    
-    static bool compare2(vector<int>& a, vector<int>& b) {
-        return a[1] < b[1];
-    }
-    
     int maxCount(int m, int n, vector<vector<int>>& ops) {
         if (ops.size() == 0)
             return m * n;
-        sort(ops.begin(), ops.end(), compare1);
-        int first = ops[0][0];
-        sort(ops.begin(), ops.end(), compare2);
-        int second = ops[0][1];
+        int first = INT_MAX, second = INT_MAX;
+        for (auto op: ops) {
+            first = min(first, op[0]);
+            second = min(second, op[1]);
+        }
         return first * second;
     }
 };

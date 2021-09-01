13f264ae0ecfd87528690dd98ba2b19760f879f1
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2021-09-01 17:52:15 +0530

    Time: 20 ms (65.81%), Space: 30.1 MB (49.29%) - LeetHub

---
+++ array-nesting/array-nesting.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        vector<bool> visited(nums.size(), false);
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!visited[i]) {
+                int start = nums[i], count = 0;
+                do {
+                    start = nums[start];
+                    count++;
+                    visited[start] = true;
+                } while (start != nums[i]);
+                res = max(res, count);
+            }
+        }
+        return res;
+    }
+};

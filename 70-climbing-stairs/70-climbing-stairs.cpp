64ff293722a8a665ce244de6e9613de2dc8e494e
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2022-05-29 19:49:34 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -7,7 +7,7 @@ public:
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }

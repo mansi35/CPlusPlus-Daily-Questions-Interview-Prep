a54ad356ff0f33c89fd4d7d7333517fd8e7671af
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2022-07-26 13:27:59 +0530

    Time: 202 ms (86.85%), Space: 77.2 MB (67.05%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,19 +5,21 @@ public:
         {
             return 0;
         }
-        stack<int> s;
-        int maxA = 0, n = heights.size();
-        for (int i = 0; i <= n; i++) {
-            while (!s.empty() && (i == n || heights[s.top()] >= heights[i])) {
-                int height = heights[s.top()];
-                s.pop();
-                int width;
-                if (s.empty()) width = i;
-                else width = i - s.top() - 1;
-                maxA = max(maxA, height * width);
+        stack<int> histogram;
+        int n = heights.size();
+        int area = 0;
+        histogram.push(0);
+        for (int i = 1; i <= n; i++) {
+            while (!histogram.empty() and (i == n || heights[i] < heights[histogram.top()])) {
+                int top = histogram.top();
+                histogram.pop();
+                if (histogram.empty())
+                    area = max(area, heights[top] * i);
+                else
+                    area = max(area, heights[top] * (i - histogram.top() - 1));
             }
-            s.push(i);
+            histogram.push(i);
         }
-        return maxA;
+        return area;
     }
 };
